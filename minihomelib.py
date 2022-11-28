16c238a94086143822afd3c05272d6244034f8ab
Chang-Yi Yen <changyiyen@gmail.com>
2022-11-28 12:02:00 +0800

    Commit various local changes

--- minihomelib.py
+++ minihomelib.py
@@ -14,10 +14,14 @@ except ImportError:
 
 # Built-ins
 import datetime
+#import sqlite3
+import sys
 
+# Change to .ini file?
 with open('config.yaml', 'r') as configfile:
     conf = yaml.safe_load(configfile)
 
+# Change to SQLite?
 with open('library.yaml', 'r') as library:
     lib = yaml.safe_load(library)
 
@@ -28,16 +32,18 @@ status = ""
 def main():
     global status
     if flask.request.method == 'GET':
-        return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, past_due=int(conf['past_due']))
+        return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, shelves=conf["shelves"], past_due=int(conf['past_due']))
     if flask.request.method == 'POST':
         # Do nothing on empty ISBN
-        if flask.request.form.get('isbn') == '':
-            return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
+        if flask.request.form.get('isbn') == '' or lib == None:
+            #return flask.redirect('/')
+            return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, shelves=conf["shelves"])
         elif flask.request.form.get('isbn') in lib:
-            #if ISBNlib_imported:
-            #    if isbnlib.notisbn(flask.request.form.get('isbn')):
-            #        status = ""Error: not valid ISBN""
-            #        return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
+            if ISBNlib_imported:
+                if isbnlib.notisbn(flask.request.form.get('isbn')):
+                    status = "Error: not valid ISBN"
+                    #return flask.redirect('/')
+                    return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
             # Checkout (start new transaction)
             if lib[flask.request.form.get('isbn')]['CHECKOUT_STATUS'] == 'checked_in':
                 lib[flask.request.form.get('isbn')]['CHECKOUT_STATUS'] = 'checked_out'
@@ -45,7 +51,8 @@ def main():
                 with open('library.yaml', 'w') as library:
                     yaml.safe_dump(lib, library)
                 status = "Book '{}' checked out!".format(lib[flask.request.form.get('isbn')]['BOOKNAME'])
-                return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
+                #return flask.redirect('/')
+                return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, shelves=conf["shelves"])
             # Checkin (close latest transaction)
             if lib[flask.request.form.get('isbn')]['CHECKOUT_STATUS'] == 'checked_out':
                 lib[flask.request.form.get('isbn')]['CHECKOUT_STATUS'] = 'checked_in'
@@ -53,17 +60,19 @@ def main():
                 with open('library.yaml', 'w') as library:
                     yaml.safe_dump(lib, library)
                 status = "Book '{}' checked in!".format(lib[flask.request.form.get('isbn')]['BOOKNAME'])
-                return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
+                #return flask.redirect('/')
+                return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, shelves=conf["shelves"])
         else:
             status = "Error: ISBN not in library"
-            return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status)
+            #return flask.redirect('/')
+            return flask.render_template('main.htm', lib=lib, pagedate=datetime.datetime.isoformat(datetime.datetime.today()), status=status, shelves=conf["shelves"])
 
 @app.route('/add', methods=['POST'])
 def add():
-    global status
+    global status, lib
     # Read stuff from web form
     isbn = flask.request.form.get('isbn_new')
-    if isbn in lib:
+    if (lib != None) and (isbn in lib):
         status = "Error: duplicate ISBN {}".format(isbn)
         return flask.redirect('/')
     bookname = flask.request.form.get('bookname')
@@ -76,6 +85,8 @@ def add():
     transaction_dates =[[datetime.datetime.today().strftime('%Y-%m-%d')]]
 
     # Initialize library entry
+    if lib == None:
+        lib = dict()
     lib[isbn] = dict()
     lib[isbn]['BOOKNAME'] = ''
     lib[isbn]['AUTHOR'] = ''
@@ -87,7 +98,7 @@ def add():
     lib[isbn]['TRANSACTION_DATES'] = transaction_dates
     
     # Automatically fetch metadata?
-    if conf['isbnlib_fetch_meta']:
+    if conf['isbnlib_fetch_meta'] and ISBNlib_imported:
         try:
             metadata = isbnlib.meta(isbn)
         except isbnlib.NotValidISBNError:
@@ -95,6 +106,7 @@ def add():
             lib.pop(isbn)
             return flask.redirect('/')
         lib[isbn]['BOOKNAME'] = metadata['Title']
+        bookname = metadata['Title']
         lib[isbn]['AUTHOR'] = ', '.join(metadata['Authors'])
     else:
         lib[isbn]['BOOKNAME'] = bookname
@@ -111,6 +123,20 @@ def add():
         yaml.safe_dump(lib, library)
     status = "Book '{}' added!".format(bookname)
     return flask.redirect('/')
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    global status, lib
+    # Load library and calculate stats
+    # Pass stats to template
+    # Generate graphs
+    ## barplot: book name / total transaction count
+    ## barplot: book name / checkout intervals
+    ## barplot: genre / total transaction count
+    ## barplot: genre / transaction count for last month
+    ## histogram: time (month) / number of books acquired for each month
+    ## histogram: time / total cumulative book count
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
